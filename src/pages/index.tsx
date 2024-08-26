9c6a78d77ef6d7fe64a58f6cfea9cc64000ecbc4
swapsatoshi <swapsatoshi@gmail.com>
2024-08-27 05:08:46 +0800

    change get started link to about swaptoshi

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,7 +23,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/category/introduction"
+            to="/introduction/about-swaptoshi"
           >
             Let's Get Started 🚀
           </Link>
